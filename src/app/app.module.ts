58e4537083eabf6ff2751a4f00e913d79093c6fe
Masaharu Komuro <komuro-m1@asahi.com>
2018-06-25 10:29:01 +0900

    一記事ページを作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AboutPageModule } from '../pages/about/about.module';
 import { ContactPageModule } from '../pages/contact/contact.module';
+import { ArticlePageModule } from '../pages/article/article.module';
 
 @NgModule({
   declarations: [
@@ -19,6 +19,7 @@ import { ContactPageModule } from '../pages/contact/contact.module';
   imports: [
     AboutPageModule,
     ContactPageModule,
+    ArticlePageModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
---
+++ src/pages/article/article.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ArticlePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage({
+  segment: 'article/:id',
+  defaultHistory: [ 'HomePage' ]
+})
+@Component({
+  selector: 'page-article',
+  templateUrl: 'article.html',
+})
+export class ArticlePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  public id: number;
+
+  ionViewDidLoad() {
+    this.id = parseInt( this.navParams.get( 'id' ), 10 );
+    console.log('ionViewDidLoad ArticlePage');
+  }
+
+}
