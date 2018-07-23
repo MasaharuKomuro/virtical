45f7d7044229b7cb5d0efbe1342d4185f57513a2
Masaharu Komuro <komuro-m1@asahi.com>
2018-07-24 00:23:46 +0900

    タブの表示ロジックを統一

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, ModalController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, ModalController, Tabs } from 'ionic-angular';
 
 @IonicPage({ segment: 'main' })
 @Component({
@@ -7,15 +7,17 @@ import { IonicPage, ModalController } from 'ionic-angular';
 })
 export class TabsPage {
 
+  @ViewChild( 'tab_group' ) tab_group: Tabs;
+  @ViewChild( Tabs ) tab_group2: Tabs;
+
   public tabRoots = [
-    { root: 'HomePage', title: 'Poker', icon: 'home' },
-    { root: 'AboutPage', title: 'About', icon: 'information-circle' },
-    { root: 'ContactPage', title: 'Contact', icon: 'contacts' }
+    { root: 'HomePage', title: ' News', icon: 'home', header_title: 'ニュース' },
+    { root: 'AboutPage', title: 'Movie', icon: 'information-circle', header_title: 'お勧め動画' },
+    { root: 'AboutPage', title: 'About', icon: 'information-circle', header_title: 'About Us' },
+    { root: 'ContactPage', title: 'Contact', icon: 'contacts', header_title: 'Contact' }
   ];
 
   constructor(
     public modalCtrl: ModalController
-    ) {
-    console.log( location.href );
-  }
+  ) {}
 }
