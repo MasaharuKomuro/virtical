293be5dea14a6b5b9d36e3bebba6003ae9081cdc
Masaharu Komuro <komuro-m1@asahi.com>
2018-07-23 23:46:10 +0900

    不要なimport文を削除

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { ModalController, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { TabsPage } from '../pages/tabs/tabs';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
@@ -21,6 +19,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+
       // statusBar.styleDefault();
       // splashScreen.hide();
 
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -10,7 +10,6 @@ export class TabsPage {
   public tabRoots = [
     { root: 'HomePage', title: 'Poker', icon: 'home' },
     { root: 'AboutPage', title: 'About', icon: 'information-circle' },
-    { root: 'ContactPage', title: 'Contact', icon: 'contacts' },
     { root: 'ContactPage', title: 'Contact', icon: 'contacts' }
   ];
 
